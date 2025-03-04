afbb4d5190eb833106129dcc350d0bd6134bc932
creallfluharty <creallfluharty@gmail.com>
2025-03-03 22:45:10 -0600

    AGENDUM-56 - clean up startdb.sh

--- startdb.sh
+++ startdb.sh
@@ -1,11 +1,12 @@
 TAG_NAME=agendum-database
 CONTAINER_NAME=agendum-database
-VOLUME_NAME="agendum-database-volume"
+VOLUME_NAME=agendum-database-volume
+PORT=27017
 
-if docker images | grep "$TAG_NAME" > /dev/null ; then
+if docker image ls | awk "match($\1, /${TAG_NAME}/) { print $\1 }" | grep . > /dev/null ; then
     echo 'image already built, skipping.'
 else
-    docker build . --tag "$TAG_NAME"
+    docker build . --tag "${TAG_NAME}"
 fi
 
-docker run --rm --detach --name "$CONTAINER_NAME" --publish 27017:27017 --volume "$VOLUME_NAME:/data/db" agendum-database
+docker run --rm --detach --name "${CONTAINER_NAME}" --publish "${PORT}:${PORT}" --volume "${VOLUME_NAME}:/data/db" "${TAG_NAME}"
