e2ecf7b0ce171e9b0800d7b211c9a92f808c7886
Maya Bowman <99492834+mbowman21@users.noreply.github.com>
2025-05-15 16:55:38 -0500

    Develop to Main (#11)
    * add create_enum helper and use it for manner_type
    
    * add status type enum
    
    * add debug option to startdb
    
    * Completion Status (#10)
    
    * tags: field -> allowed value
    
    * move create_enum to db_utils, add status field to default task type
    
    ---------
    
    Co-authored-by: creallfluharty <creallfluharty@gmail.com>
    Co-authored-by: creallfluharty <32851582+creallfluharty@users.noreply.github.com>

--- db_initialization.py
+++ db_initialization.py
@@ -1,41 +1,15 @@
 from db_python_util.db_classes import TaskType, Field, ValueType, AllowedValue
-from db_python_util.db_helper import ConnectionManager
+from db_python_util.db_helper import ConnectionManager, create_enum
 
 
 # function that creates the default task type and saves it to the database
 @ConnectionManager.requires_connection
 def createDefaultTaskType():
+    tag_value_type, tags = create_enum("Tag", [])
 
-    # create a Tag Value Type
-    tag_value_type = ValueType(
-        name="Tag",
-        allowed_values=[]
-    )
-    tag_value_type.save()
+    manner_type, manners = create_enum("Manner", ["Blocking", "Subtask"])
 
-    # create a Manner Value Type
-    manner_value_type = ValueType(
-        name="Manner",
-        allowed_values=[]
-    )
-    manner_value_type.save()
-
-    # create Blocking and Subtask Fields using Manner Value Type
-    blocking_manner = AllowedValue(
-        value="Blocking",
-        value_type=manner_value_type,
-    )
-    blocking_manner.save()
-
-    subtask_manner = AllowedValue(
-        value="Subtask",
-        value_type=manner_value_type,
-    )
-    subtask_manner.save()
-
-    manner_value_type.allowed_values = [blocking_manner, subtask_manner]
-    manner_value_type.save()
-
+    status_type, statuses = create_enum("Status", ["Not Started", "In Progress", "Complete"])
 
     # create default ValueTypes: string and user
     string_value_type = ValueType(
@@ -92,12 +66,25 @@ def createDefaultTaskType():
         default_allowed_value=None,
         value_type=string_value_type
     )
+    tags_system_field = Field(
+        name="Tags",
+        min_values=0,
+        value_type=tag_value_type,
+    )
+    status_system_field = Field(
+        name="Status",
+        min_values=1,
+        max_values=1,
+        value_type=status_type,
+    )
     name_system_field.save()
     assignee_system_field.save()
     description_system_field.save()
     due_date_system_field.save()
     x_location_system_field.save()
     y_location_system_field.save()
+    tags_system_field.save()
+    status_system_field.save()
 
     # create default task type
     default_task_type = TaskType(
@@ -109,7 +96,9 @@ def createDefaultTaskType():
             description_system_field,
             due_date_system_field,
             x_location_system_field,
-            y_location_system_field
+            y_location_system_field,
+            tags_system_field, # TODO: unlike other fields, static tags should be combined rather than overriden (but static fields in general are out of scope right now)
+            status_system_field,
         ],
         static_field_values=[],
         workspaces=[],
